381f8fb903b0eeb507f78e3d028167445dba07d7
williamsears <wpeter1@hotmail.com>
2015-11-26 17:41:48 +0000

    Exercise 1 which displays the list of databases

--- boilerplate.js
+++ boilerplate.js
@@ -7,18 +7,20 @@ var connection = mysql.createConnection({
   database : 'addressbook'
 });
 
-connection.query("SELECT * FROM Account", function(err, rows, fields) {
+connection.connect();
+
+connection.query("show databases", function(err, rows, fields) {
   // In this callback, rows will be all the rows of the query, in a regular array of regular objects
   // fields is not used very often, but it will contain a listing of the columns with some metadata
-  
+  if (err) throw err;
   // Here is an example usage:
   rows.forEach(function(row) {
-    console.log('#' + row.id + ': ' + row.email);
+    console.log(row.Database);
   });
   // This code will output lines like:
   // #1: john@smith.com
   // #2: abc@def.com
   // #5: xx@yy.com
-  
+  connection.end();
   // Note that IDs do not have to be contiguous. If we DELETE rows, there will be holes in the ID list. This is normal.
 });
