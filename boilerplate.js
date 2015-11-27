e0f796e1e893dfe7c3ca3308671c528045d14267
williamsears <wpeter1@hotmail.com>
2015-11-27 15:04:05 +0000

    init

--- boilerplate.js
+++ boilerplate.js
@@ -1,55 +1,19 @@
+
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: process.env.IP,
-  user: process.env.C9_USER,
-  password: '',
-  database: 'addressbook'
+ host     : process.env.IP,
+ user     : process.env.C9_USER,
+ password : '',
+ database : 'addressbook'
 });
 
-connection.query("select * from Account join AddressBook on AddressBook.accountId=Account.id join Entry on Entry.addressBookId=AddressBook.id;", function(err, rows, fields) {
-  // In this callback, rows will be all the rows of the query, in a regular array of regular objects
-  // fields is not used very often, but it will contain a listing of the columns with some metadata
-  if (err) throw err;
-  // Here is an example usage:
-  var user = [];
-  for (var i = 0; i < rows.length; i++) {
-    var counter = 0;
-    var obj = {
-      arr: []
-    };
-    var currentIndex;
-    for (var j = 0; j < user.length; j++) {
-      if (user[j].ActId === rows[i].ActId) {
-        counter++
-        currentIndex = j;
-      }
-    }
-    if (counter < 1) {
-      obj.ActId = rows[i].ActId;
-      obj.Email = rows[i].AccountEmail;
-      obj.arr.push(rows[i].AddressBookName);
-      user.push(obj);
-    }
-    else {
-      user[currentIndex].arr.push(rows[i].AddressBookName);
-    }
+connection.query("SELECT Account.id AS actId, Account.email AS actEmail, AddressBook.id AS adrId, AddressBook.accountId AS adrActId, AddressBook.name AS adrName, Entry.id AS entId, Entry.addressbookId AS entAdrId, Entry.firstName AS first, Entry.lastName AS last FROM Account JOIN AddressBook ON Account.id=AddressBook.accountId JOIN Entry ON Entry.addressbookId=AddressBook.id", function(err, rows, fields) {
+ if(err) throw err;
+ console.log(rows);
 
-  }
-  for (var k = 0; k < user.length; k++) {
-    console.log("-------------");
-    console.log(user[k].Email)
-    if (user[k].arr[0] === null) {
-      console.log("---No Address Books---")
-    } else {
-    
-    console.log(user[k].arr.join(", "));
-    }
-  }
-  // This code will output lines like:
-  // #1: john@smith.com
-  // #2: abc@def.com
-  // #5: xx@yy.com
+ var user = [];
+ for (var i = 0; )
+ 
 
-  // Note that IDs do not have to be contiguous. If we DELETE rows, there will be holes in the ID list. This is normal.
-});
+ });
